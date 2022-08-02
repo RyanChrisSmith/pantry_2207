53372b55e443f9b6e876029492172aa6b0ab37a9
Ryan Smith <ryan.chris.smith@gmail.com>
2022-08-02 09:04:34 -0600

    test: ingredient class

---
+++ spec/ingredient_spec.rb
@@ -0,0 +1,14 @@
+require './lib/pantry'
+
+RSpec.describe Ingredient do
+
+  before :each do
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+  end
+
+  it 'exists' do
+    expect(@ingredient1).to be_a(Ingredient)
+  end
+
+
+end
