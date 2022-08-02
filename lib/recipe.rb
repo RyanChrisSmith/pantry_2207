a95e687beef0b1877fa62a2f56df3c93256cdc20
Ryan Smith <ryan.chris.smith@gmail.com>
2022-08-02 10:25:54 -0600

    add ingredients method

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,8 +1,18 @@
 class Recipe
-  attr_reader :name
+  attr_reader :name,
+              :ingredients_required
 
   def initialize(name)
     @name = name
+    @ingredients_required = {}
+  end
+
+  def add_ingredient(ingredient, quantity)
+    if @ingredients_required.has_key?(ingredient)
+      @ingredients_required[ingredient] += quantity
+    else
+      @ingredients_required[ingredient] = quantity
+    end
   end
 
 end
--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -2,7 +2,7 @@ require './lib/ingredient'
 require './lib/pantry'
 require './lib/recipe'
 
-RSpec.describe Recipe do 
+RSpec.describe Recipe do
 
   before :each do
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
@@ -16,6 +16,21 @@ RSpec.describe Recipe do
 
   it 'has attributes' do
     expect(@recipe1.name).to eq "Mac and Cheese"
+    expect(@recipe1.ingredients_required).to eq ({})
+  end
+
+  it 'can add ingredients' do
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    expect(@recipe1.ingredients_required).to eq ({@ingredient1 => 6, @ingredient2 => 8})
+  end
+
+  it 'can list the ingredients without quantities' do
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    expect(@recipe1.ingredients).to eq([ingredient1, ingredient2])
   end
 
 end
